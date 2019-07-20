743d8bc383e3e9f248bbd998e3e44faa0efaa14f
PhuChau <vinhphu101195@gmail.com>
2019-07-20 12:19:20 +0300

    update Yield

--- Yield.js
+++ Yield.js
@@ -1,32 +1,47 @@
-function  *main(){
-    yield 1;
-    yield 2;
-    yield 3;
+// function  *main(){
+//     yield 1;
+//     yield 2;
+//     yield 3;
+// }
+
+// var it = main();
+
+// it.next(); // {value :1 , done: false}
+// it.next();// {value :2 , done: false}
+// it.next();// {value :3 , done: false}
+
+// it.next();// {value :undefined , done: true}
+
+function coroutine(g) {
+  var it = g();
+  return function() {
+    return it.next.apply(it, arguments);
+  };
 }
 
-var it = main();
+// var run = coroutine(function *(){
+//     var x   = 1 + (yield);
+//     var y = 1 + (yield);
+//     yield( x + y);
+// });
 
-it.next(); // {value :1 , done: false}
-it.next();// {value :2 , done: false}
-it.next();// {value :3 , done: false}
+// run();
+// run(10);
+// console.log("Meaning of life: " + run(30).value);
 
-it.next();// {value :undefined , done: true}
-
-
-
-function coroutine(g){
-    var it = g();
-    return function(){
-        return it.next.apply(it,arguments);
-    }
+function getData(d) {
+  setTimeout(function() {
+    run(d);
+  }, 1000);
 }
 
-var run = coroutine(function *(){
-    var x   = 1 + (yield);
-    var y = 1 + (yield);
-    yield( x + y);
+var run = coroutine(function*() {
+  console.log("start");
+
+  var x = 1 + (yield getData(10));
+  var y = 1 + (yield getData(30));
+  var ans = yield getData("ans: " + (x + y));
+  console.log(ans);
 });
 
 run();
-run(10);
-console.log("Meaning of life: " + run(30).value);
